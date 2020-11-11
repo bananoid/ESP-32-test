b31804a85696da40b2586dc4b9b3b4471136e2c9
Giosuè Russo <josh@hizen.io>
2020-11-12 02:04:52 +0900

    ESP-now work

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,194 @@
 // https://randomnerdtutorials.com/esp-now-one-to-many-esp32-esp8266/
 
+// #include <WiFi.h>
+
+// 1 = AC:67:B2:2C:C6:60
+// 2 = AC:67:B2:2C:B7:3C
+// 3 = AC:67:B2:2C:80:FC
+// 4 = AC:67:B2:2C:76:E0
+
+// void setup()
+// {
+//   Serial.begin(9600);
+//   while (Serial.available())
+//   {
+//   }
+
+//   Serial.println();
+//   Serial.print("ESP Board MAC Address:  ");
+//   Serial.println(WiFi.macAddress());
+// }
+
+// void loop()
+// {
+//   Serial.println(WiFi.macAddress());
+// }
+
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#define IS_MASTER
+
+#ifdef IS_MASTER
+
+#include <esp_now.h>
 #include <WiFi.h>
+
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = {0xAC, 0x67, 0xB2, 0x2C, 0xB7, 0x3C}; // 2
+uint8_t broadcastAddress2[] = {0xAC, 0x67, 0xB2, 0x2C, 0x80, 0xFC}; // 3
+uint8_t broadcastAddress3[] = {0xAC, 0x67, 0xB2, 0x2C, 0x76, 0xE0}; // 4
+
+typedef struct test_struct
+{
+  int x;
+  int y;
+} test_struct;
+
+test_struct test;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void setup()
 {
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  // register peer
+  esp_now_peer_info_t peerInfo;
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  // register first peer
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // register second peer
+  memcpy(peerInfo.peer_addr, broadcastAddress2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+  /// register third peer
+  memcpy(peerInfo.peer_addr, broadcastAddress3, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void loop()
+{
+  test.x = random(0, 20);
+  test.y = random(0, 20);
+
+  esp_err_t result = esp_now_send(0, (uint8_t *)&test, sizeof(test_struct));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  delay(16);
+}
+
+#else
+
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+//Structure example to receive data
+//Must match the sender structure
+typedef struct test_struct
+{
+  int x;
+  int y;
+} test_struct;
+
+//Create a struct_message called myData
+test_struct myData;
+
+//callback function that will be executed when data is received
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("x: ");
+  Serial.println(myData.x);
+  Serial.print("y: ");
+  Serial.println(myData.y);
   Serial.println();
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+}
+
+void setup()
+{
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop()
 {
 }
+
+#endif
